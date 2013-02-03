0a45bbfbdaf35f72400eca916feceb687f81a0d7
jmccaf <mccaffrey.jonathan@gmail.com>
2013-02-02 20:37:55 -0800

    Got MediaEffect running with no blatent errors, still nothing appearing.

--- src/clojure/com/mccaffrey/nlve/ImageEffectActivity.clj
+++ src/clojure/com/mccaffrey/nlve/ImageEffectActivity.clj
@@ -24,11 +24,19 @@
       (log-i (str '~val '~'is x#))
       x#))
 
+
 (defmacro do-let
   [[binding-form init-expr] & body]
   `(let [~binding-form ~init-expr]
      ~@body
      ~binding-form))
+
+(defmacro do-rev
+  "Kind of like do, but return the first expr in the body, instead of the last.
+  Useful for appending logging/debug statements to calculations."
+  [first-expr & rest]
+  `(do-let [res# ~first-expr]
+           ~@rest))
 
 (defmacro n-ints-from-buffer
   [n body]
@@ -60,6 +68,17 @@
     :dynamic true}
   *debug-gl-shader-log* true)
 
+(def 
+  ^{:doc "Obsessively check GL errors after each call.
+          Disable for performance.
+          Enable for sanity."
+    :dynamic true}
+  *debug-gl-check-errors* true)
+
+(def 
+  ^{:doc "Throw an exception whenever a GL error occurs."
+    :dynamic true}
+  *debug-exception-on-gl-errors* true)
 
 (def 
   ^{:doc "Shrink fs rects to 3/4ths size of debugging"
@@ -75,10 +94,18 @@
     (onFrameAvailable [this surface-texture]
       (boolean (handler-fn surface-texture)))))
 
+; Functions to deal with the camera parameters
 (defmacro on-frame-available
   [& body]
   `(on-frame-available-call
      (fn [~'surface-texture] ~@body)))
+
+(defn get-preview-size
+  [cam] 
+  (let [sz (.. cam
+             (getParameters)
+             (getPreviewSize))]
+    [(.width sz) (.height sz)]))
 
 (defn get-display-rotation
   [activity]
@@ -108,6 +135,7 @@
       :default
       (mod (+ (- (.orientation info) degrees) 360) 360))))
 
+; Functions to deal with up-loading data to and from OpenGL
 (def float-sz
   "Size in bytes of a floating point value"
    4)
@@ -141,7 +169,6 @@
     (.put (float-array in))
     (.position 0)))
 
-
 (defn vec-to-ubyte-buffer
   [in]
   (doto (ByteBuffer/allocateDirect (* (count in) ubyte-sz))
@@ -149,73 +176,96 @@
     (.put (byte-array (map ubyte-ify in)))
     (.position 0)))
 
+; functions to deal with GL errors and debugging
+(defn gl-get-actual-error
+  "Get a non-trivial GL error."
+  []
+  (let [errno (GLES20/glGetError)]
+    (if (not= errno GLES20/GL_NO_ERROR)
+      (format "%02x" errno) nil)))
+
+(defn print-gl-errors 
+  "Print all current GL errors."
+  [& op]
+  (while
+    (when-let [errno (gl-get-actual-error)]
+      (if (and errno *debug-exception-on-gl-errors*)
+        (throw (Throwable. (str "GL error: " errno))))
+      (log-i (if op
+               (str "GL errors after " op " " errno)
+               (str "GL error: " errno))))))
+
+(defn print-gl-errors-checked
+  "Printf all current gl errors if 
+  *debug-gl-check-errors* is true"
+  []
+  (if *debug-gl-check-errors* (print-gl-errors)))
+
+(defmacro gl-calls
+  "Wrap a sequence of GL call expression,
+  optionally checking GL errors after each.
+  Returns the value of the last expr."
+  ([call-form]
+   `(do-rev
+      ~call-form
+      (if *debug-gl-check-errors* (print-gl-errors))))
+  ([call-form & more]
+   `(do ~call-form
+       (if *debug-gl-check-errors* (print-gl-errors))
+       (gl-calls ~@more))))
+
+; Functions to deal with creating and rendering OpenGL objects
 (defn setup-attrib
   [kw]
   (log-i (str kw))
-  (GLES20/glEnableVertexAttribArray (:location kw))
+  (gl-calls (GLES20/glEnableVertexAttribArray (:location kw)))
   (apply #(GLES20/glVertexAttribPointer %1 %2 %3 %4 %5 %6)
          ((juxt :location :size :type :norm :stride :buffer) kw)))
 
 (defn gen-texture
+  "Return a texture name created by glGenTextures"
   []
-  (let [textures (make-array Integer/TYPE 1)]
-    (GLES20/glGenTextures 1 textures 0)
-    (nth textures 0)))
+  {:name 
+   (let [textures (int-array 1)]
+     (gl-calls (GLES20/glGenTextures 1 textures 0))
+     (nth textures 0))})
 
 (defn make-texture 
   [target]
-  (let [name (gen-texture)]
-    (doto target
-      (GLES20/glBindTexture name)
-      ; TODO revisit filtering choice
-      (GLES20/glTexParameterf GLES20/GL_TEXTURE_MIN_FILTER GLES20/GL_NEAREST)
-      (GLES20/glTexParameterf GLES20/GL_TEXTURE_MAG_FILTER GLES20/GL_NEAREST)
-      (GLES20/glTexParameteri GLES20/GL_TEXTURE_WRAP_S GLES20/GL_CLAMP_TO_EDGE)
-      (GLES20/glTexParameteri GLES20/GL_TEXTURE_WRAP_T GLES20/GL_CLAMP_TO_EDGE))
-    name))
+  (do-let [tex (gen-texture)]
+     (doto target
+       (GLES20/glBindTexture (:name tex))
+       ; TODO revisit filtering choice
+       (GLES20/glTexParameterf GLES20/GL_TEXTURE_MIN_FILTER GLES20/GL_LINEAR)
+       (GLES20/glTexParameterf GLES20/GL_TEXTURE_MAG_FILTER GLES20/GL_LINEAR)
+       (GLES20/glTexParameteri GLES20/GL_TEXTURE_WRAP_S GLES20/GL_CLAMP_TO_EDGE)
+       (GLES20/glTexParameteri GLES20/GL_TEXTURE_WRAP_T GLES20/GL_CLAMP_TO_EDGE))
+     (print-gl-errors-checked)))
 
-(defn gl-get-actual-error
-  []
-  (let [errno (GLES20/glGetError)]
-    (if (not= errno GLES20/GL_NO_ERROR) errno nil)))
-
-(defn print-gl-errors 
-  "Print all current GL errors."
-  []
-  (while
-    (when-let [errno (gl-get-actual-error)]
-      (log-i (str errno)))))
-
-(defn make-2x2-texture 
-  []
-  (do-let [name (make-texture GLES20/GL_TEXTURE_2D)]
-    (GLES20/glBindTexture GLES20/GL_TEXTURE_2D name)
-    (GLES20/glTexImage2D
-      GLES20/GL_TEXTURE_2D
-      0
-      GLES20/GL_RGBA
-      2
-      2
-      0
-      GLES20/GL_RGBA
-      GLES20/GL_UNSIGNED_BYTE
-      (vec-to-ubyte-buffer
-        [255 255 255 255
-         255   0   0 255
-         0   255   0 255
-         0     0 255 255]
-;        [0 0 0 255
-;         0   0   0 255
-;         0   0   0 255
-;         0     0 0 255]
-        ))))
-
-(defn get-preview-size
-  [cam] 
-  (let [sz (.. cam
-             (getParameters)
-             (getPreviewSize))]
-    [(.width sz) (.height sz)]))
+  (defn make-2x2-texture 
+    []
+    (assoc (do-let [tex (make-texture GLES20/GL_TEXTURE_2D)]
+                   (gl-calls 
+                     (GLES20/glBindTexture GLES20/GL_TEXTURE_2D (tex :name))
+                     (GLES20/glTexImage2D
+                       GLES20/GL_TEXTURE_2D
+                       0
+                       GLES20/GL_RGBA
+                       2
+                       2
+                       0
+                       GLES20/GL_RGBA
+                       GLES20/GL_UNSIGNED_BYTE
+                       (vec-to-ubyte-buffer
+                         [255 255 255 255
+                          255   0   0 255
+                          0   255   0 255
+                          0     0 255 255]))))
+           ;        [0 0 0 255
+           ;         0   0   0 255
+           ;         0   0   0 255
+           ;         0     0 0 255]
+           :width 2 :height 2))
 
 ; (defn request-window-features!
 ;   "Requests the given features for the activity.  The features should be
@@ -320,36 +370,42 @@
   "Calls useProgram and binds *program* to program"
   [program & body]
   `(let [program# ~program]
-     (GLES20/glUseProgram program#)
+     (gl-calls (GLES20/glUseProgram program#))
      (binding [*program* program#]
        ~@body)))
 
 (defn assoc-location
+  "Associate an attribute location to a map representing a vertex attribute."
   [kw loc-str]
-  (assoc kw :location (GLES20/glGetAttribLocation *program* loc-str)))
+  (assoc kw :location (gl-calls (GLES20/glGetAttribLocation *program* loc-str))))
 
 (defn setup-texture
-  [tex-name unit uniform-name]
-  (GLES20/glActiveTexture (+ GLES20/GL_TEXTURE0 unit))
-  (GLES20/glBindTexture GLES20/GL_TEXTURE_2D tex-name)
-  (GLES20/glUniform1i
-    (GLES20/glGetUniformLocation *program* uniform-name)
-    unit))
+  "Setup a texture for rendering with a program."
+  [tex unit uniform-name]
+  (gl-calls 
+    (GLES20/glActiveTexture (+ GLES20/GL_TEXTURE0 unit))
+    (GLES20/glBindTexture GLES20/GL_TEXTURE_2D (tex :name))
+    (GLES20/glUniform1i
+      (GLES20/glGetUniformLocation *program* uniform-name)
+      unit)))
 
 (defn draw-fs-tex-rect
-  [tex-name]
-  (log-i "Trying to Draw A rect")
-  (setup-texture tex-name 0 "uSampler")
+  "Draw a given texture on full-screen quad."
+  [tex]
+  (setup-texture tex 0 "uSampler")
   (setup-attrib (assoc-location fs-pos-attrib "aPosition"))
   (setup-attrib (assoc-location fs-tex-attrib "aTexcoord0"))
-  (GLES20/glDrawArrays GLES20/GL_TRIANGLE_FAN 0 4))
+  (gl-calls (GLES20/glDrawArrays GLES20/GL_TRIANGLE_FAN 0 4)))
 
 (defn load-shader
+  "Create a shader of a given type from a string of source code,
+  returning the compiled shader object."
   [type-code src]
   (do-let
     [shader (doto (GLES20/glCreateShader type-code)
               (GLES20/glShaderSource src)
               (GLES20/glCompileShader))]
+    (print-gl-errors-checked)
     (when (and *debug-gl-shader-log*
                (= GLES20/GL_FALSE
                   (n-ints-from-buffer 1
@@ -357,17 +413,27 @@
       (log-i (GLES20/glGetShaderInfoLog shader)))))
 
 (defn load-program
+  "Create a program from vertex and fragment shader source code,
+  returning the linked program object."
   [vtx-src frg-src]
   (do-let [program
-  (doto (GLES20/glCreateProgram)
-    (GLES20/glAttachShader (load-shader GLES20/GL_VERTEX_SHADER vtx-src))
-    (GLES20/glAttachShader (load-shader GLES20/GL_FRAGMENT_SHADER frg-src))
-    (GLES20/glLinkProgram))]
+           (doto (GLES20/glCreateProgram)
+             (GLES20/glAttachShader (load-shader GLES20/GL_VERTEX_SHADER vtx-src))
+             (GLES20/glAttachShader (load-shader GLES20/GL_FRAGMENT_SHADER frg-src))
+             (GLES20/glLinkProgram))]
+          (print-gl-errors-checked)
           (when (and *debug-gl-shader-log*
                      (= GLES20/GL_FALSE
                         (n-ints-from-buffer 1
                           (GLES20/glGetProgramiv program GLES20/GL_LINK_STATUS))))
             (log-i (GLES20/glGetProgramInfoLog program)))))
+
+(defn make-effect
+  "Wrapper for MediaEffect creation"
+  [factory str-name]
+  {:pre [(EffectFactory/isEffectSupported str-name)]}
+  (log-i (str "Making effect " str-name))
+  (? (.createEffect factory str-name)))
 
 (defn make-preview-renderer
   [gl-surface-view rotation]
@@ -375,55 +441,75 @@
     (reify android.opengl.GLSurfaceView$Renderer
       (onDrawFrame 
         [_ gl10] 
-        (GLES20/glClearColor 0.8 0.3 0.3 1.0)
-        (GLES20/glClear GLES20/GL_COLOR_BUFFER_BIT)
+        (gl-calls
+          (GLES20/glClearColor 0.8 0.3 0.3 1.0)
+          (GLES20/glClear GLES20/GL_COLOR_BUFFER_BIT))
+        (.apply (@gl-state :saturate)
+                (get-in @gl-state [:preview-tex :name])
+                (get-in @gl-state [:preview-tex :width])
+                (get-in @gl-state [:preview-tex :height])
+                (get-in @gl-state [:display-tex :name]))
         (with-program (@gl-state :simple-prg)
-          (draw-fs-tex-rect (@gl-state :preview-tex))))
+          (draw-fs-tex-rect (@gl-state :display-tex))))
       (onSurfaceChanged
         [_ gl10 w h] )
       (onSurfaceCreated
         [renderer gl10 egl-cfg] 
-        (let [cam-id 0
-              cam (Camera/open cam-id)
-              [w h] (get-preview-size cam)
-              preview-tex (make-2x2-texture)
-              ;preview-tex (make-texture GLES11Ext/GL_TEXTURE_EXTERNAL_OES)
-              preview-st (doto (SurfaceTexture. preview-tex)
-                           (.setOnFrameAvailableListener
-                             (on-frame-available
-                               (log-i "preview frame incoming")
-                               (.requestRender gl-surface-view))))
-              simple-prg (load-program
-                           "attribute vec2 aPosition;
-                           attribute vec2 aTexcoord0;
+          (let [; cam-id 0
+                ; cam (Camera/open cam-id)
+                ; [w h] (get-preview-size cam)
+                preview-tex (make-2x2-texture)
+                display-tex (gen-texture)
+                preview-st (doto (SurfaceTexture. (preview-tex :name))
+                             (.setOnFrameAvailableListener
+                               (on-frame-available
+                                 (log-i "preview frame incoming")
+                                 (.requestRender gl-surface-view))))
+                saturate (do-let 
+                           [saturate (-> (EffectContext/createWithCurrentGlContext)
+                                       .getFactory
+                                       (make-effect EffectFactory/EFFECT_SEPIA))]
+                           (.apply saturate
+                                   (preview-tex :name)
+                                   (preview-tex :width)
+                                   (preview-tex :height)
+                                   (display-tex :name)))
+                simple-prg (load-program
+                             "attribute vec2 aPosition;
+                             attribute vec2 aTexcoord0;
 
-                           varying vec2 vTexcoord0;
+                             varying vec2 vTexcoord0;
 
-                           void main() {
+                             void main() {
                              gl_Position = vec4(aPosition, 0, 1);
                              // Check to pass texcoord from v -> f
                              //vTexcoord0 = vec2(1.0, 0.0);
                              vTexcoord0 = aTexcoord0;
-                           }"
-                           "precision mediump float;
-                           varying vec2 vTexcoord0;
-                           uniform sampler2D uSampler;
+                             }"
+                             "precision mediump float;
+                             varying vec2 vTexcoord0;
+                             uniform sampler2D uSampler;
 
-                           void main() {
-                              gl_FragColor = texture2D(uSampler, vTexcoord0);
-                              //gl_FragColor = syntax err
+                             void main() {
+                             gl_FragColor = texture2D(uSampler, vTexcoord0);
+                             //gl_FragColor = syntax err
                              // Check to make sure texcoord is valid
                              // gl_FragColor = vec4(vTexcoord0,1,1);
-                           }")]
-          (GLES20/glEnable GLES20/GL_TEXTURE_2D)
-          (doto cam
-            (.setPreviewTexture preview-st)
-           ; (.setDisplayOrientation
-           ;   (front-facing-camera-correction cam-id rotation))
-            (.startPreview))
-          (dosync (ref-set
-            gl-state {:preview-tex preview-tex
-                      :simple-prg simple-prg})))))))
+                             }")]
+            ; Enabling GL_TEXTURE_2D is not needed in ES 2.0.
+            ; (gl-calls (GLES20/glEnable GLES20/GL_TEXTURE_2D))
+            
+            ; (.setParameter saturate "scale" (float -1))
+ ;           (doto cam
+ ;             (.setPreviewTexture preview-st)
+ ;             ; (.setDisplayOrientation
+ ;             ;   (front-facing-camera-correction cam-id rotation))
+ ;             (.startPreview))
+            (dosync (ref-set
+                      gl-state {:preview-tex preview-tex
+                                :display-tex display-tex
+                                :saturate saturate
+                                :simple-prg simple-prg})))))))
 
 (defn -onCreate
   [^Activity this ^Bundle bundle] 
@@ -437,7 +523,8 @@
         (.setEGLContextClientVersion 2)
         (.setPreserveEGLContextOnPause false) ; start with the more bug-prone mode
         (.setEGLConfigChooser false) ; no depth
-        (.setDebugFlags GLSurfaceView/DEBUG_CHECK_GL_ERROR)
+        ; Annoyingly, this is broken for ES2 funcs
+        ; (.setDebugFlags GLSurfaceView/DEBUG_CHECK_GL_ERROR)
         (.setRenderer (make-preview-renderer
                         preview-surface
                         (get-display-rotation *activity*)))
